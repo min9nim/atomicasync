197a39105cf794e089dfc6aea7f1395c3f5636ab
mgsong <mgsong@jjsoft.kr>
2019-12-28 01:12:53 +0900

    update type

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,4 +1,4 @@
 declare module "atomicasync" {
-  function atomicasync(arg: (...args: any[]) => Promise<any>): Promise<any>
+  function atomicasync(arg: (...args: any[]) => Promise<any>): (...args: any[]) => Promise<any>
   export = atomicasync
 }
