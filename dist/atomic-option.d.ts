68afc884fcf65684717719084bbd8b56185b38e1
min9nim <min9nim@gmail.com>
2020-02-04 19:52:49 +0900

    add dist to .gitignore

--- dist/atomic-option.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function useAddAtomicOption(): (fn: (...args: any[]) => Promise<any>, thisObj?: any) => (args?: any) => any;
--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './atomic-option';
-export default function atomic(asyncFn: (...args: any[]) => Promise<any>, thisObj?: any): (...args: any[]) => any;
